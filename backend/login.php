98cc23f300859478ae1d7cb055bcd8553a41e0c3
unknown <if23.muhammadirham@mhs.ubpkarawang.ac.id>
2024-11-27 16:14:48 +0700

    first commit

--- backend/login.php
+++ backend/login.php
@@ -1,13 +1,29 @@
 <?php
-        if (isset($_POST['email']) && isset($_POST['password'])) {
-            $email = $_POST['email']; 
-            $password = $_POST['password'];
 
-            if (!empty($email) && !empty($password)) {
-                header('Location: ./../dashboard.html');
-                exit();
-            } else {
-                echo "<p style='color:red;'>Please enter both email and password</p>";
-            }
+require './../config/db.php';
+
+if(isset($_POST['submit'])) {
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $user = mysqli_query($db_connect,"SELECT * FROM users WHERE email = '$email'");
+    if(mysqli_num_rows($user) > 0) {
+        $data = mysqli_fetch_assoc($user);
+        
+        if(password_verify($password,$data['password'])) {
+            echo "selamat datang ".$data['name'];
+            die;
+
+            //otorisasi
+        } else {
+            echo "password salah";
+            die;
         }
-    ?>
+
+    } else {
+        echo "email atau password salah";
+        die;
+    }
+}
+?>
